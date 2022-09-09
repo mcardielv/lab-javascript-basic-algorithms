6fc8c74540a19ecf8ded692f320cade4c6e436d4
Miquel Cardiel <mcardielv@hotmail.com>
2022-09-09 18:01:25 +0200

    added index.html to refer index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,32 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Mikey";
+const hacker2 = "Spike";
+
+
+// console.log(`the driver's name is ${ hacker1 }`);
+// console.log(`The navigation's name is ${hacker2}`);
+
+// const number1 = parseInt (prompt("first number"));
+
 // Iteration 2: Conditionals
+// if (hacker1.length > hacker2.length) {
+//     console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+// }
+// else if (hacker1.length < hacker2.length) {
+//     console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters., `)
+// }
+// else if (hacker1.length == hacker2.length){
+//     console.log(`Wow you have equally long names, ${hacker1.length} characters!`);
+// };
 
 
 // Iteration 3: Loops
+let hacker1Splitted = hacker1.split("-");
+
+// for (i = 0; i <= hacker1.length; i++)
+// {
+//     let hacker1Split = hacker1.split(i);
+// }
+console.log(hacker1Splitted);
+
